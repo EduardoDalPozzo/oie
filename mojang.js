65f4a5e6942e742a0af49501c3cb0c16103d90b2
Du <110917469+EduardoDalPozzo@users.noreply.github.com>
2023-06-22 09:09:54 -0300

    Add files via upload

---
+++ mojang.js
@@ -0,0 +1,26 @@
+var nome= (window.prompt('teu nome?'))
+
+         
+function changeColor(color) {
+    document.body.style.background = color;
+}
+ 
+function verdão() {
+    changeColor('green');
+    el_down.innerHTML = "Background Color changed";
+}       
+
+
+
+function changeColor(color) {
+    document.body.style.background = color;
+}
+ 
+function vermei() {
+    changeColor('#c11');
+    el_down.innerHTML = "Background Color changed";
+}       
+
+function Nome() {
+ window.alert('Nome completo:' + nome)
+}       
